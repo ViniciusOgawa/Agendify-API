05db91f661c2cbe994e950670793059bf44014d9
ViniciusOgawa <vini_ogawa@hotmail.com>
2023-05-27 02:26:32 -0300

    fix create contact route

--- src/routers/contacts.routes.ts
+++ src/routers/contacts.routes.ts
@@ -1,18 +1,35 @@
-import { Router } from "express"
-import { deleteContactController, listContactController, updateContactController, createContactController } from "../controllers/contacts.controllers"
-import { ensureTokenIsValid } from "../middlewares/ensureTokenIsValid.middleware"
-import { ensureDataIsValid } from "../middlewares/ensureDataIsValid.middleware"
-import { contactSchemaRequest, contactSchemaUpdate } from "../schemas/contact.schema"
-import { ensureEmailExists } from "../middlewares/ensureEmailExists.middleware"
-import { ensureIsOwnerMiddleware } from "../middlewares/ensureIsOwner.middleware"
+import { Router } from "express";
+import {
+  deleteContactController,
+  listContactController,
+  updateContactController,
+  createContactController,
+} from "../controllers/contacts.controllers";
+import { ensureTokenIsValid } from "../middlewares/ensureTokenIsValid.middleware";
+import { ensureDataIsValid } from "../middlewares/ensureDataIsValid.middleware";
+import {
+  contactSchemaRequest,
+  contactSchemaUpdate,
+} from "../schemas/contact.schema";
+import { ensureEmailExists } from "../middlewares/ensureEmailExists.middleware";
+import { ensureIsOwnerMiddleware } from "../middlewares/ensureIsOwner.middleware";
 
-const contactsRoutes: Router = Router()
+const contactsRoutes: Router = Router();
 
-contactsRoutes.use(ensureTokenIsValid)
+contactsRoutes.use(ensureTokenIsValid);
 
-contactsRoutes.post("", ensureEmailExists, ensureDataIsValid(contactSchemaRequest), createContactController)
-contactsRoutes.delete("/:id", ensureIsOwnerMiddleware, deleteContactController)
-contactsRoutes.patch("/:id", ensureIsOwnerMiddleware, ensureDataIsValid(contactSchemaUpdate), updateContactController)
-contactsRoutes.get("", listContactController)
+contactsRoutes.post(
+  "",
+  ensureDataIsValid(contactSchemaRequest),
+  createContactController
+);
+contactsRoutes.delete("/:id", ensureIsOwnerMiddleware, deleteContactController);
+contactsRoutes.patch(
+  "/:id",
+  ensureIsOwnerMiddleware,
+  ensureDataIsValid(contactSchemaUpdate),
+  updateContactController
+);
+contactsRoutes.get("", listContactController);
 
-export { contactsRoutes }
+export { contactsRoutes };
